e6d28e904b1a3c0a335d2ba36df35c0693147d8c
Leonardo Costa Gomes <leonardocosta0910@gmail.com>
2025-11-05 09:11:11 -0300

    Add files via upload

--- feira_matematica/EncriptionManager.php
+++ feira_matematica/EncriptionManager.php
@@ -4,5 +4,9 @@ require_once("CaesarCipher.php");
 require_once("BitwiseNegation.php");
 require_once("EncriptionWithMatrices.php");
 
-$encriptions = [new CaesarCipher(), new BitwiseNegation(), new EncriptionWithMatrices()];
+$encriptions = [
+    0 => new CaesarCipher(),
+    1 => new BitwiseNegation(), // A Negação Bit a Bit geralmente não usa chave
+    2 => new EncriptionWithMatrices() // Assumindo que esta é a que usa EncriptionWithKey
+];
 ?>
--- feira_matematica/test.php
+++ feira_matematica/test.php
@@ -1,13 +1,23 @@
-<?php
-require_once("Encription.php");
-require_once("EncriptionWithKey.php");
-require_once("CaesarCipher.php");
-require_once("BitwiseNegation.php");
-require_once("EncriptionWithMatrices.php");
-    $encriptType = new EncriptionWithMatrices();
-    if ($encriptType instanceof EncriptionWithKey)
-    {
-        $encriptType->key = "Punhos transparentes";
-    }
-    echo $encriptType->encript("Raul");
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste</title>
+</head>
+
+<body>
+    <?php
+    require_once("Encription.php");
+    require_once("EncriptionWithKey.php");
+    require_once("CaesarCipher.php");
+    require_once("BitwiseNegation.php");
+    require_once("EncriptionWithMatrices.php");
+    include("GenerateForm.php");
+
+    echo GetForm();
+    ?>
+</body>
+
+</html>
